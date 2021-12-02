6faa29f637706df420b3c0dfea77648239fc4afb
Kevin Zingg <95413708+KevinViZi@users.noreply.github.com>
2021-12-02 11:49:36 +0100

    Add files via upload
    Operatoren Aufgabe

---
+++ Operator2/src/Main.java
@@ -0,0 +1,57 @@
+// Defines package
+
+// Imports libraries
+import static java.lang.System.*;   // System-IO Library for abbriviation purpose to static functions
+import static java.lang.Math.*;     // Mathematic Library for abbriviation purpose to static functions
+import java.util.*;                 // Random is part of this library
+
+// Imports Input functions
+//import static Input.*;       // All the functions can be used now!
+
+//Every program must be placed in a class ...
+public class Main {
+
+    // Our main function which runs the program12
+    public static void main(String[] args ) {
+
+        // Title
+        System.out.println("Operator:");
+        out.println();
+
+        // Input
+        double dblX = Input.inputDouble("Geben Sie die ertse Zahl ein: ");
+        double dblY = Input.inputDouble("Geben Sie die zweite Zahl ein: ");
+
+
+        //Calculations
+        double plus = (dblX + dblY);
+        double minus1 = (dblX - dblY);
+        double minus2 = (dblY - dblX);
+        double times = (dblX * dblY);
+        double divide1 = (dblX / dblY);
+        double divide2 = (dblY % dblX);
+
+        //output
+        out.println("Das Resultat von 1.Zahl + 2.Zahl ist: " + plus);
+        out.println("Das Resultat von 1.Zahl - 2.Zahl ist: " + minus1);
+        out.println("Das Resultat von 2.Zahl - 1.Zahl ist: " + minus2);
+        out.println("Das Resultat von 1.Zahl * 2.Zahl ist: " + times);
+        out.println("Das Resultat von 1.Zahl geteilt durch 2.Zahl ist: " + divide1);
+        out.println("Das Resultat von 2.Zahl geteilt durch 1.Zahl ist: " + divide2);
+
+
+
+
+        // Input
+        double dblZ = Input.inputDouble("Geben Sie eine dritte Zahl ein: ");
+
+        //output
+        out.println("Das Resultat von 1.Zahl + 2.Zahl ist: " + (plus += dblZ));
+        out.println("Das Resultat von 1.Zahl - 2.Zahl ist: " + (minus1 -= dblZ));
+        out.println("Das Resultat von 2.Zahl - 1.Zahl ist: " + (minus2 -= dblZ));
+        out.println("Das Resultat von 1.Zahl * 2.Zahl ist: " + (times *= dblZ));
+        out.println("Das Resultat von 1.Zahl geteilt durch 2.Zahl ist: " + (divide1 /= dblZ));
+        out.println("Das Resultat von 2.Zahl geteilt durch 1.Zahl ist: " + (divide2 /= dblZ));
+
+    }
+}
